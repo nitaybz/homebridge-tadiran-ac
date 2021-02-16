676d7e7bce1587a739c0619188cc1b2c24f6304b
nitaybz <nitaybz@gmail.com>
2021-02-16 23:03:25 +0200

    fix eslint issues

--- index.js
+++ index.js
@@ -6,189 +6,191 @@ const PLUGIN_NAME = 'homebridge-tadiran-ac';
 const PLATFORM_NAME = 'TadiranAC';
 
 const CLASS_DEF = {
-    airconditioner: AirConditionerAccessory
+	airconditioner: AirConditionerAccessory
 };
 
 let Characteristic, PlatformAccessory, Service, Categories, UUID;
 
 module.exports = function(homebridge) {
-    ({
-        platformAccessory: PlatformAccessory,
-        hap: {Characteristic, Service, Accessory: {Categories}, uuid: UUID}
-    } = homebridge);
+	({
+		platformAccessory: PlatformAccessory,
+		hap: {Characteristic, Service, Accessory: {Categories}, uuid: UUID}
+	} = homebridge);
 
-    homebridge.registerPlatform(PLUGIN_NAME, PLATFORM_NAME, TadiranAC, true);
+	homebridge.registerPlatform(PLUGIN_NAME, PLATFORM_NAME, TadiranAC, true);
 };
 
 class TadiranAC {
-    constructor(...props) {
-        [this.log, this.config, this.api] = [...props];
+	constructor(...props) {
+		[this.log, this.config, this.api] = [...props];
 
-        this.cachedAccessories = new Map();
-        if(!this.config || !this.config.devices) {
-            this.log("No devices found. Check that you have specified them in your config.json file.");
-            return false;
-        }
+		this.cachedAccessories = new Map();
+		if(!this.config || !this.config.devices) {
+			this.log("No devices found. Check that you have specified them in your config.json file.");
+			return false;
+		}
 
-        this._expectedUUIDs = this.config.devices.map(device => UUID.generate(PLUGIN_NAME +(device.fake ? ':fake:' : ':') + device.id));
+		this._expectedUUIDs = this.config.devices.map(device => UUID.generate(PLUGIN_NAME +(device.fake ? ':fake:' : ':') + device.id));
 
-        this.api.on('didFinishLaunching', () => {
-            this.discoverDevices();
-        });
-    }
+		this.api.on('didFinishLaunching', () => {
+			this.discoverDevices();
+		});
+	}
 
-    discoverDevices() {
-        const devices = {};
-        const connectedDevices = [];
-        const fakeDevices = [];
-        this.config.devices.forEach(device => {
-            try {
-                device.id = ('' + device.id).trim();
-                device.key = ('' + device.key).trim();
-                device.type = 'airconditioner';
+	discoverDevices() {
+		const devices = {};
+		const connectedDevices = [];
+		const fakeDevices = [];
+		this.config.devices.forEach(device => {
+			try {
+				device.id = ('' + device.id).trim();
+				device.key = ('' + device.key).trim();
+				device.type = 'airconditioner';
 
-                device.ip = ('' + (device.ip || '')).trim();
-            } catch(ex) {}
+				device.ip = ('' + (device.ip || '')).trim();
+			} catch(ex) {
+				// 
+			}
 
-            //if (!/^[0-9a-f]+$/i.test(device.id)) return this.log.error('%s, id for %s, is not a valid id.', device.id, device.name || 'unnamed device');
-            if (!/^[0-9a-f]+$/i.test(device.key)) return this.log.error('%s, key for %s (%s), is not a valid key.', device.key.replace(/.{4}$/, '****'), device.name || 'unnamed device', device.id);
-            if (!{16:1, 24:1, 32: 1}[device.key.length]) return this.log.error('%s, key for %s (%s), doesn\'t have the expected length.', device.key.replace(/.{4}$/, '****'), device.name || 'unnamed device', device.id);
+			//if (!/^[0-9a-f]+$/i.test(device.id)) return this.log.error('%s, id for %s, is not a valid id.', device.id, device.name || 'unnamed device');
+			if (!/^[0-9a-f]+$/i.test(device.key)) return this.log.error('%s, key for %s (%s), is not a valid key.', device.key.replace(/.{4}$/, '****'), device.name || 'unnamed device', device.id);
+			if (!{16:1, 24:1, 32: 1}[device.key.length]) return this.log.error('%s, key for %s (%s), doesn\'t have the expected length.', device.key.replace(/.{4}$/, '****'), device.name || 'unnamed device', device.id);
 
-            if (device.fake) fakeDevices.push({name: device.id.slice(8), ...device});
-            else devices[device.id] = {name: device.id.slice(8), ...device};
-        });
+			if (device.fake) fakeDevices.push({name: device.id.slice(8), ...device});
+			else devices[device.id] = {name: device.id.slice(8), ...device};
+		});
 
-        const deviceIds = Object.keys(devices);
-        if (deviceIds.length === 0) return this.log.error('No valid configured devices found.');
+		const deviceIds = Object.keys(devices);
+		if (deviceIds.length === 0) return this.log.error('No valid configured devices found.');
 
-        this.log.info('Starting discovery...');
+		this.log.info('Starting discovery...');
 
-        TuyaDiscovery.start({ids: deviceIds})
-            .on('discover', config => {
-                if (!config || !config.id) return;
-                if (!devices[config.id]) return // this.log.warn('Discovered a device that has not been configured yet (%s@%s).', config.id, config.ip);
+		TuyaDiscovery.start({ids: deviceIds})
+			.on('discover', config => {
+				if (!config || !config.id) return;
+				if (!devices[config.id]) return // this.log.warn('Discovered a device that has not been configured yet (%s@%s).', config.id, config.ip);
 
-                connectedDevices.push(config.id);
+				connectedDevices.push(config.id);
 
-                this.log.info('Discovered %s (%s) identified as %s (%s)', devices[config.id].name, config.id, 'Air Conditioner', config.version);
+				this.log.info('Discovered %s (%s) identified as %s (%s)', devices[config.id].name, config.id, 'Air Conditioner', config.version);
 
-                const device = new TuyaAccessory({
-                    ...devices[config.id], ...config,
-                    UUID: UUID.generate(PLUGIN_NAME + ':' + config.id),
-                    connect: false
-                });
-                this.addAccessory(device);
-            });
+				const device = new TuyaAccessory({
+					...devices[config.id], ...config,
+					UUID: UUID.generate(PLUGIN_NAME + ':' + config.id),
+					connect: false
+				});
+				this.addAccessory(device);
+			});
 
-        fakeDevices.forEach(config => {
-            this.log.info('Adding fake device: %s', config.name);
-            this.addAccessory(new TuyaAccessory({
-                ...config,
-                UUID: UUID.generate(PLUGIN_NAME + ':fake:' + config.id),
-                connect: false
-            }));
-        });
+		fakeDevices.forEach(config => {
+			this.log.info('Adding fake device: %s', config.name);
+			this.addAccessory(new TuyaAccessory({
+				...config,
+				UUID: UUID.generate(PLUGIN_NAME + ':fake:' + config.id),
+				connect: false
+			}));
+		});
 
-        setTimeout(() => {
-            deviceIds.forEach(deviceId => {
-                if (connectedDevices.includes(deviceId)) return;
+		setTimeout(() => {
+			deviceIds.forEach(deviceId => {
+				if (connectedDevices.includes(deviceId)) return;
 
-                if (devices[deviceId].ip) {
+				if (devices[deviceId].ip) {
 
-                    this.log.info('Failed to discover %s (%s) in time but will connect via %s.', devices[deviceId].name, deviceId, devices[deviceId].ip);
+					this.log.info('Failed to discover %s (%s) in time but will connect via %s.', devices[deviceId].name, deviceId, devices[deviceId].ip);
 
-                    const device = new TuyaAccessory({
-                        ...devices[deviceId],
-                        UUID: UUID.generate(PLUGIN_NAME + ':' + deviceId),
-                        connect: false
-                    });
-                    this.addAccessory(device);
-                } else {
-                    this.log.warn('Failed to discover %s (%s) in time but will keep looking.', devices[deviceId].name, deviceId);
-                }
-            });
-        }, 60000);
-    }
+					const device = new TuyaAccessory({
+						...devices[deviceId],
+						UUID: UUID.generate(PLUGIN_NAME + ':' + deviceId),
+						connect: false
+					});
+					this.addAccessory(device);
+				} else {
+					this.log.warn('Failed to discover %s (%s) in time but will keep looking.', devices[deviceId].name, deviceId);
+				}
+			});
+		}, 60000);
+	}
 
-    registerPlatformAccessories(platformAccessories) {
-        this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, Array.isArray(platformAccessories) ? platformAccessories : [platformAccessories]);
-    }
+	registerPlatformAccessories(platformAccessories) {
+		this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, Array.isArray(platformAccessories) ? platformAccessories : [platformAccessories]);
+	}
 
-    configureAccessory(accessory) {
-        if (accessory instanceof PlatformAccessory && this._expectedUUIDs.includes(accessory.UUID)) {
-            this.cachedAccessories.set(accessory.UUID, accessory);
-            accessory.services.forEach(service => {
-                if (service.UUID === Service.AccessoryInformation.UUID) return;
-                service.characteristics.some(characteristic => {
-                    if (!characteristic.props ||
+	configureAccessory(accessory) {
+		if (accessory instanceof PlatformAccessory && this._expectedUUIDs.includes(accessory.UUID)) {
+			this.cachedAccessories.set(accessory.UUID, accessory);
+			accessory.services.forEach(service => {
+				if (service.UUID === Service.AccessoryInformation.UUID) return;
+				service.characteristics.some(characteristic => {
+					if (!characteristic.props ||
                         !Array.isArray(characteristic.props.perms) ||
                         characteristic.props.perms.length !== 3 ||
                         !(characteristic.props.perms.includes(Characteristic.Perms.WRITE) && characteristic.props.perms.includes(Characteristic.Perms.NOTIFY))
-                    ) return;
+					) return;
 
-                    this.log.info('Marked %s unreachable by faulting Service.%s.%s', accessory.displayName, service.displayName, characteristic.displayName);
+					this.log.info('Marked %s unreachable by faulting Service.%s.%s', accessory.displayName, service.displayName, characteristic.displayName);
 
-                    characteristic.updateValue(new Error('Unreachable'));
-                    return true;
-                });
-            });
-        } else {
-            /*
+					characteristic.updateValue(new Error('Unreachable'));
+					return true;
+				});
+			});
+		} else {
+			/*
              * Irrespective of this unregistering, Homebridge continues
              * to "_prepareAssociatedHAPAccessory" and "addBridgedAccessory".
              * This timeout will hopefully remove the accessory after that has happened.
              */
-            setTimeout(() => {
-                this.removeAccessory(accessory);
-            }, 1000);
-        }
-    }
+			setTimeout(() => {
+				this.removeAccessory(accessory);
+			}, 1000);
+		}
+	}
 
-    addAccessory(device) {
-        const deviceConfig = device.context;
-        const type = 'airconditioner';
+	addAccessory(device) {
+		const deviceConfig = device.context;
+		const type = 'airconditioner';
 
-        const Accessory = CLASS_DEF[type];
+		const Accessory = CLASS_DEF[type];
 
-        let accessory = this.cachedAccessories.get(deviceConfig.UUID),
-            isCached = true;
+		let accessory = this.cachedAccessories.get(deviceConfig.UUID),
+			isCached = true;
 
-        if (accessory && accessory.category !== Accessory.getCategory(Categories)) {
-            this.log.info("%s has a different type (%s vs %s)", accessory.displayName, accessory.category, Accessory.getCategory(Categories));
-            this.removeAccessory(accessory);
-            accessory = null;
-        }
+		if (accessory && accessory.category !== Accessory.getCategory(Categories)) {
+			this.log.info("%s has a different type (%s vs %s)", accessory.displayName, accessory.category, Accessory.getCategory(Categories));
+			this.removeAccessory(accessory);
+			accessory = null;
+		}
 
-        if (!accessory) {
-            accessory = new PlatformAccessory(deviceConfig.name, deviceConfig.UUID, Accessory.getCategory(Categories));
-            accessory.getService(Service.AccessoryInformation)
-                .setCharacteristic(Characteristic.Manufacturer, 'Tadiran')
-                .setCharacteristic(Characteristic.Model, 'IOT Connect')
-                .setCharacteristic(Characteristic.SerialNumber, deviceConfig.id.slice(8));
+		if (!accessory) {
+			accessory = new PlatformAccessory(deviceConfig.name, deviceConfig.UUID, Accessory.getCategory(Categories));
+			accessory.getService(Service.AccessoryInformation)
+				.setCharacteristic(Characteristic.Manufacturer, 'Tadiran')
+				.setCharacteristic(Characteristic.Model, 'IOT Connect')
+				.setCharacteristic(Characteristic.SerialNumber, deviceConfig.id.slice(8));
 
-            isCached = false;
-        }
+			isCached = false;
+		}
 
-        if (accessory && accessory.displayName !== deviceConfig.name) {
-            this.log.info(
-                "Configuration name %s differs from cached displayName %s. Updating cached displayName to %s ",
-                deviceConfig.name, accessory.displayName, deviceConfig.name);
-            accessory.displayName = deviceConfig.name;
-        }
+		if (accessory && accessory.displayName !== deviceConfig.name) {
+			this.log.info(
+				"Configuration name %s differs from cached displayName %s. Updating cached displayName to %s ",
+				deviceConfig.name, accessory.displayName, deviceConfig.name);
+			accessory.displayName = deviceConfig.name;
+		}
 
-        this.cachedAccessories.set(deviceConfig.UUID, new Accessory(this, accessory, device, !isCached));
-    }
+		this.cachedAccessories.set(deviceConfig.UUID, new Accessory(this, accessory, device, !isCached));
+	}
 
-    removeAccessory(homebridgeAccessory) {
-        if (!homebridgeAccessory) return;
+	removeAccessory(homebridgeAccessory) {
+		if (!homebridgeAccessory) return;
 
-        this.log.warn('Unregistering', homebridgeAccessory.displayName);
+		this.log.warn('Unregistering', homebridgeAccessory.displayName);
 
-        delete this.cachedAccessories[homebridgeAccessory.UUID];
-        this.api.unregisterPlatformAccessories(PLATFORM_NAME, PLATFORM_NAME, [homebridgeAccessory]);
-    }
+		delete this.cachedAccessories[homebridgeAccessory.UUID];
+		this.api.unregisterPlatformAccessories(PLATFORM_NAME, PLATFORM_NAME, [homebridgeAccessory]);
+	}
 
-    removeAccessoryByUUID(uuid) {
-        if (uuid) this.removeAccessory(this.cachedAccessories.get(uuid));
-    }
+	removeAccessoryByUUID(uuid) {
+		if (uuid) this.removeAccessory(this.cachedAccessories.get(uuid));
+	}
 }
